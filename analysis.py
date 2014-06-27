37585fdf54eb7c59e05f5a0047c64df604568655
Dpo <dporret@free.fr>
2014-06-27 13:59:37 +0200

    backup

--- analysis.py
+++ analysis.py
@@ -1,24 +1,38 @@
 import skrf as rf
 from pylab import *
+import os
 
-touchstone_file_lf = 't520_10u_lfF.s1p'
-touchstone_file= 't520_10uF.s1p'
+data_folder='results'
+touchstone_file_lf = os.path.join(data_folder,'t520_10u_lfF.s1p')
+touchstone_file = os.path.join(data_folder,'t520_10uF.s1p')
+baseline= os.path.join(data_folder,'T520B476M010ATE035.s2p')
+
+# Network 1
 zmeas_lf = rf.Network(touchstone_file_lf)
 zmeas=rf.Network(touchstone_file)
 
+full_ntwk=rf.stitch(zmeas_lf[2:], zmeas)
+full_ntwk.name='T520B_47uF10V_meas'
 
+# Network 2
+model_ntwk=rf.Network(baseline)
+model_ntwk.name="T520B_47uF10V_model"
+
+# Saving Network 1
+full_ntwk.write_touchstone('T520B_47uF10V_meas.s1p','./results/',False,True)
+
+# Plotting
 rf.stylely() # matplotlib custom style from skrf.mplstyle
-figure(figsize=(8,4))
-subplot(221)
-s11.plot_s_db()
-title('Z11 Magnitude')
-subplot(222)
-s11.plot_s_deg()
-title('Z11 Phase')
-tight_layout()
-subplot(223)
-title('Z mag')
+figure(figsize=(8,6))
 loglog()
-s11.plot_z_mag(marker='x', markevery=1)
 
+model_ntwk.plot_z_mag(m=0,n=0,marker='x', markevery=1, label='model')
+full_ntwk.plot_z_mag(marker='+', markevery=1,label='measured')
+title('Kemet T520B 47uF 10V' )
+ylim([1e-2,1e2])
+xlim([1e2,1e9])
+# Save plot
+rf.save_all_figs(dir='./results/', format=['png'],echo=True)
 show()
+
+
